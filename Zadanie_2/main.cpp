bb546ae3ed1f0559a8aed3683ec630713ff5bf4f
Zhenya Nikiforov <12A92@mail.ru>
2026-03-30 18:54:45 +0500

    Улучшенное второе

--- Zadanie_2/main.cpp
+++ Zadanie_2/main.cpp
@@ -2,62 +2,34 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <functional>
 #include <future>
 using namespace std;
 
-int multiply(int integerNumber)
+void showNumber(int n)
 {
-  return integerNumber * 2;
+  cout << n << ", ";
 }
 
-void customForEach(int start, int end, vector<int> &integerNumbers, function<int(int)> fn)
+void customForEach(vector<int>::iterator str, vector<int>::iterator stp, function<void(int)> fn)
 {
-  if (start == end)
+  if (distance(str, stp) < 5)
   {
-    future<int> result = async(fn, integerNumbers[start]);
-    integerNumbers[start] = result.get();
-  }
-  else if (start + 1 == end)
-  {
-    customForEach(start, start, integerNumbers, multiply);
-    customForEach(end, end, integerNumbers, multiply);
-  }
-  else if (start + 2 == end)
-  {
-    customForEach(start, start, integerNumbers, multiply);
-    customForEach(end - 1, end, integerNumbers, multiply);
+    for_each(str, stp, fn);
   }
   else
   {
-    int space = ((end - 1) - start);
-    int halfSpace = space / 2;
-    int remainderSpace = space % 2;
-    customForEach(start, (start + halfSpace), integerNumbers, multiply);
-    customForEach((end - halfSpace - remainderSpace), end, integerNumbers, multiply);
+    size_t halfSize = distance(str, stp) / 2;
+    future<void> result = async(customForEach, str, str + halfSize, fn);
+    result.get();
+    customForEach(str + halfSize, stp, fn);
   }
-}
-
-void showIntegerNumbers(vector<int> &integerNumbers)
-{
-  for (int i = 0; i < integerNumbers.size(); ++i)
-  {
-    cout << integerNumbers[i] << ", ";
-  }
-  cout << endl;
 }
 
 int main()
 {
-  vector<int> integerNumbers{1, 2, 3, 4, 5};
-
-  cout << "Do:" << endl;
-  showIntegerNumbers(integerNumbers);
-
-  customForEach(0, integerNumbers.size() - 1, integerNumbers, multiply);
-
-  cout << "Posle:" << endl;
-  showIntegerNumbers(integerNumbers);
-
+  vector<int> integerNumbers{1, 2, 3, 4, 5, 6, 7, 8, 9};
+  customForEach(integerNumbers.begin(), integerNumbers.end(), showNumber);
   return 0;
 }
